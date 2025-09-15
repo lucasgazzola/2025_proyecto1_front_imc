464da2713d8652c0d876bcb02904dfded81fd778
Lautaro Guajardo <lautaroguajardo4@gmail.com>
2025-09-14 22:37:32 -0300

    Fix duplicado

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/'
 
 // TODO: Extraer rutas de la API a este archivo
 export const API = {
