87cc83a210a7930c02258ee81897cbf369e7cc10
Lautaro Guajardo <lautaroguajardo4@gmail.com>
2025-09-14 21:26:10 -0300

    Definir URL base y rutas iniciales de la API

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/'
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
+// TODO: Extraer rutas de la API a este archivo
 export const API = {
   IMC: {
     CALCULAR: `${API_URL}/imc/calcular`,
